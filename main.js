a61954849e2636d6ccce8841deeb50f900038691
Joey Sagnes <dragonskymine@gmail.com>
2018-02-01 14:26:59 +0100

    system template
    ajout fonction template handlebars

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var handlebars = require('handlebars');
+var fs = require('fs');
+
+function loadHtml(page, data) {
+  fs.readFile(page+'.html', function(err, source) {
+    var template = Handlebars.compile(source);
+    var result = template(data);
+    return result;
+  });
+}
 
 
 var app = express();
 
 
 app.get('/', function(req, res) {
-    res.render('/layout/index.ejs');
+  res.render('/layout/index.ejs');
 });
 
 app.listen(8080);
